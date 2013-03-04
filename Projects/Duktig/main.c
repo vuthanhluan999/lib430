fc4681ef8adced054f2748fddfc67c87c27e9b44
GG430s <gg430s@googlemail.com>
2013-03-04 20:26:56 +0000

    Duktig Updated to V1.0.3 without PWM. This requires R1 and R8 to be changed to 560 Ohms

--- Projects/Duktig/main.c
+++ Projects/Duktig/main.c
@@ -1,8 +1,8 @@
 /**********************************************************************************************\
 * Filename:    Main.c                                                                          *
-* Description: Duktig                                                                          *
+* Description: Duktig - This code requires R1 and R8 to be changed to 560 Ohms                 *
 *                                                                                              *
-*                                      MSP430F2012                                             *
+*                                      MSP430F2011                                             *
 *                              /|\  -----------------                                          *
 *                               |  |                 |                                         *
 *                               ---|DVCC         DVSS|--|                                      *
@@ -11,19 +11,19 @@
 *                                  |                 |           |                             *
 *                                  |              RST|----R47k----                             *
 *                                  |                 |                                         *
-*                        LED2------|P1.4         P1.7|------KEY2                               *
+*                        LED2------|P1.4         P1.7|------KEY1                               *
 *                                  |                 |                                         *
-*                        LED1------|P1.5         P1.6|------KEY1                               *
+*                        LED1------|P1.5         P1.6|------KEY2                               *
 *                                  |                 |                                         *
 *                                   -----------------                                          *
 *                                                                                              *
-* Device:   MSP430F2012                                                                        *
-* Version:  1.0.2                                                                              *
+* Device:   MSP430F2011                                                                        *
+* Version:  1.0.3                                                                              *
 * Compiler: IAR Embedded Workbench IDE V.5.51.3                                                *
 *                                                                                              *
 * COPYRIGHT:                                                                                   *
 * Author:   Gerald Gradl                                                                       *
-* Date:     02/2013                                                                            *
+* Date:     03/2013                                                                            *
 *                                                                                              *
 * This program is free software: you can redistribute it and/or modify it under the terms of   *
 * the GNU General Public License as published by the Free Software Foundation, either          *
@@ -41,16 +41,12 @@
 
 #define LED1 BIT4
 #define LED2 BIT5
-#define KEY1 BIT7
 #define KEY2 BIT6
-#define ON   1
-#define OFF  0
+#define KEY1 BIT7
 
-unsigned char LEDstate1;
-unsigned char LEDstate2;
 unsigned int time;
 
-#define Version 0x0102
+#define Version 0x0103
 #define VersionLocation 0x0FFFF-RESET_VECTOR-3
 __root static const int x@VersionLocation = Version;
 
@@ -80,37 +76,6 @@ int main(void) {
         while(1)
         {
         __low_power_mode_4();
-        __no_operation();
-            while(1)
-            {
-              if(LEDstate1 == ON)
-              {
-                P1OUT |= LED1;
-              }
-              if(LEDstate2 == ON)
-              {
-                P1OUT |= LED2;
-              }
-              for(unsigned int j=0;j<0x250;j++){}
-              if(LEDstate1 == ON)
-              {
-                P1OUT &= ~LED1;
-              }
-              if(LEDstate2 == ON)
-              {
-                P1OUT &= ~LED2;
-              }
-              for(unsigned int j=0;j<0x500;j++){}
-              if((time>=100) || ((LEDstate1 == OFF) && (LEDstate2 == OFF)))                 // Actual seconds is roughly time x 3
-              {
-              time=0;
-              WDTCTL = WDTPW + WDTHOLD;
-              LEDstate1 = OFF;
-              LEDstate2 = OFF;
-              P1OUT &= ~(LED1+LED2);
-              break;
-              }
-            }
         }
 }
 
@@ -123,27 +88,33 @@ __interrupt void ISR_WDT(void)
 {
         IFG1 &= ~WDTIFG;              // Clear WDT interrupt flag
         time++;
+        if(time>=20)                 // Actual seconds is roughly time x 3
+        {
+        time=0;
+        _BIS_SR_IRQ(OSCOFF);
+        WDTCTL = WDTPW + WDTHOLD;
+        P1OUT &= ~(LED1+LED2);
+        }
+
 }
 
 #pragma vector=PORT1_VECTOR
 __interrupt void Port_1(void)
 {
-        
-        P1OUT &= ~(LED1 + LED2);
-        for(unsigned int i=0;i<2;i++){for(unsigned int j=0;j<0xFFFF;j++){}}  //Debounce
-        if(P1IFG & KEY1)
+        for(unsigned int i=0;i<2;i++){for(unsigned int j=0;j<0xA000;j++){}}  //Debounce
+        if(P1IFG == KEY1)
         {
-        LEDstate1 ^= 1;
+        P1OUT ^= LED1;
         }
-        if(P1IFG & KEY2)
+        else
         {
-        LEDstate2 ^= 1;
+        P1OUT ^= LED2;
         }
         P1IFG = 0;
-        time=0;
-        if(1 == LEDstate1 | LEDstate2)
+
+        if(P1OUT == LED1 | LED2)
         {
         WDTCTL = WDTPW + WDTTMSEL + WDTCNTCL + WDTSSEL; // watchdog counter mode, ACLK, /32768
-        _BIC_SR_IRQ(OSCOFF+CPUOFF+SCG1+SCG0);
+        _BIC_SR_IRQ(OSCOFF);
         }
 }
